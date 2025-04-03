3031a48240d1e621e0b677086bb8d28b2830738e
Yutman <kuriapeter95@gmail.com>
2025-04-04 00:59:55 +0300

    Fetch and display random quotes from API -Created 'newQuote' function to pick and display a random quote - Added 'getQuotes' function to fetch quotes from the API and call 'newQuote'

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,1 +1,23 @@
-console.log('Hello, World! This is a test script.');
+let apiQuotes = []; // I'm using let here instead of a const because I will be reassigning the variable later
+// Show new quote
+function newQuote() {
+    //  Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+//  Get quotes from the API
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+   try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+       newQuote();
+   } catch (error) {
+        // Handle error
+   }
+}
+
+// On load
+getQuotes();
